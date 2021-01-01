43597603afad435eca7a9bf072f3e976a72dfb99
Xin <xin2001yan@gmail.com>
2020-12-31 17:54:42 -0800

    update home page

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -5,6 +5,7 @@ import {
 
 } from '@material-ui/core'
 import Carousel from 'react-material-ui-carousel'
+import { Link } from 'react-router-dom'
 
 
 
@@ -26,8 +27,73 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.down('xs')]: {
             height: 200
         }
+    },
+    nav: {
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap"
+    },
+    centered: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        userSelect: "none",
+        "&:hover": {
+            cursor: "pointer"
+        },
+        color: "white",
+        fontSize: "3rem",
+    },
+    container: {
+        position: "relative",
+        left: 0,
+        top: 0,
+        width: 500,
+        height: 300,
+        margin: 30,
+        [theme.breakpoints.down("xs")]: {
+            margin: 0,
+            width: "100%",
+            height: undefined
+        }
+    },
+    a: {
+        position: "relative",
+        top: 0,
+        left: 0,
+        aspectRatio: 3 / 5,
+        width: "100%",
+        height: "100%",
+    },
+    arrowLeft: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        aspectRatio: 3 / 5,
+        width: "100%",
+        height: "100%",
+        opacity: 0,
+        "&:hover": {
+            opacity: 0.5,
+            cursor: "pointer"
+        },
+    },
+    arrowRight: {
+        transform: "rotate(180deg)",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        aspectRatio: 3 / 5,
+        width: "100%",
+        height: "100%",
+        opacity: 0,
+        "&:hover": {
+            opacity: 0.5,
+            cursor: "pointer"
+        },
     }
-}));
+}))
 
 
 export default function Home() {
@@ -45,12 +111,12 @@ export default function Home() {
         },
         {
             title: "Electronics Section",
-            description: "The Electronics section is now available and could be accessed via the sidebar",
+            description: "The Electronics section is now available",
             image: "https://static.thehoneycombers.com/wp-content/uploads/sites/2/2017/05/electronics-.jpg"
         },
         {
             title: "Clothing & Fashion Section",
-            description: "The new Clothing & Fashion section is now available",
+            description: "The Clothing & Fashion section is now available",
             image: "https://img5.goodfon.com/wallpaper/nbig/0/4d/odezhda-veshalka-ruka.jpg"
         }
     ]
@@ -60,6 +126,40 @@ export default function Home() {
             <Carousel className={classes.root}>
                 {items.map((item, i) => <Item item={item} key={i} />)}
             </Carousel>
+
+            <div className={classes.nav}>
+                <Link to="/electronics">
+                    <div className={classes.container}>
+                        <img
+                            src="https://static.thehoneycombers.com/wp-content/uploads/sites/2/2017/05/electronics-.jpg"
+                            alt="bg"
+                            className={classes.a}
+                        />
+                        <div className={classes.centered}>Electronics</div>
+                        <img
+                            src="https://image.flaticon.com/icons/png/512/120/120900.png"
+                            alt="out"
+                            className={classes.arrowLeft}
+                        />
+                    </div>
+                </Link>
+
+                <Link to="/clothing-and-fashion">
+                    <div className={classes.container}>
+                        <img
+                            src="https://img5.goodfon.com/wallpaper/nbig/0/4d/odezhda-veshalka-ruka.jpg"
+                            alt="bg"
+                            className={classes.a}
+                        />
+                        <div className={classes.centered}>Clothing & Fashion</div>
+                        <img
+                            src="https://image.flaticon.com/icons/png/512/120/120900.png"
+                            alt="out"
+                            className={classes.arrowRight}
+                        />
+                    </div>
+                </Link>
+            </div>
         </React.Fragment>
     )
 }
--- src/redux/slice/slice.js
+++ src/redux/slice/slice.js
@@ -82,7 +82,6 @@ export const slice = createSlice({
                 delete newCart[action.payload.id]
             }
             state.cart = newCart
-            // TODO: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
         }
     }
 })
